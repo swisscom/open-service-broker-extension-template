74a7d3e1a5a6e31514741aeb03ed1514b82e51b0
Murat Celep <murat.celep@swisscom.com>
2017-11-01 11:33:51 +0100

    add example unit test

---
+++ broker/src/test/groovy/com/example/sb/CredentialsSpec.groovy
@@ -0,0 +1,28 @@
+package com.example.sb
+
+import org.skyscreamer.jsonassert.JSONAssert
+import spock.lang.Specification
+
+class CredentialsSpec extends Specification {
+
+    def "Verify json serialization works correctly"() {
+        given:
+        Credentials credentials = new Credentials(username: 'username',
+                                            password:'password',
+                                            host:'host',
+                                            custom: 'custom')
+
+        and:
+        String expected = """{
+                                "credentials": {
+                                    "username": "username",
+                                    "password": "password",
+                                    "host": "host",
+                                    "custom": "custom"
+                                }
+                             }"""
+
+        expect:
+        JSONAssert.assertEquals(expected, credentials.toJson(), true)
+    }
+}
